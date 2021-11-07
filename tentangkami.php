d9a3fdd9615e78c1c6cf712d28389d4734508877
Gustoc <56625312+gustogg@users.noreply.github.com>
2021-11-07 22:18:34 +0700

    tes

--- tentangkami.php
+++ tentangkami.php
@@ -1,5 +1,5 @@
 <?php include 'layout/header.php'; ?>
-<title>Home</title>
+<title>Tentang Kami</title>
 <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 <style>
     .swiper-pagination-bullet-active {
@@ -10,11 +10,29 @@
 
 <body>
     <?php include 'layout/navbar.php' ?>
-    <div class="pt-40 pb-40">
-                <p class="text-sm text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+    <div class="pt-40 mb-5">
+                <p class="text-center text-3xl font-bold ">Yayasan Akhsara Linia Indonesia</p>
+                <img class="pt-5 bg-white shadow-md rounded-md w-48 mx-auto pt-8" src="/images/logo.png" alt="YALI logo">
+                <p class="pt-10 text-center text-xl text-red-500 font-bold">Filosofi Logo</p>
+                <ul class="bg-white shadow-md rounded-md pb-10 list-disc md:list-disc text-center">
+                  <li class="pt-3 pb-3">•	Buku yang terbuka sebagai symbol bahwa buku adalah sumber ilmu dan membaca itu sebagai proses mendulang ilmu.</li>
+                    <li class="pt-3 pb-3">• Lambang bola dunia sebagai simbol dimana membaca adalah wahana melihat dunia</li>
+                    <li class="pt-3 pb-5">• Warna emas pada bola dunia sebagai simbol dimana ilmu sebagai sarana untuk menggapai emas.</li>
+                    </ul>
             </div>
+    <div class="mt-10 mb-10 bg-white shadow-md rounded-md">
+                <p class="text-red-500 text-center text-xl font-bold">Tentang Kami</p>
+                <p class="pt-3 px-40 text-justify pb-10">Yayasan Akshara Linia Indonesia adalah Yayasan yang bergerak dibidang sosial, bidang kemanusiaan dengan akta Notaris Amaliyah, SH., M.KN., Nomor 13 tanggal 09 Maret 2021 dan Keputusan Mentri Hukum Dan Hak Asasi Manusia Republik Indonesia, Nomor AHU-0010105.AH.01.12 Tahun 2021 tanggal 17 Maret 2021 serta Nomor Induk Berusaha (NIB) 1238000301296 tanggal 19 Maret 2021.</p>
+
+    </div>
+    <div class="mt-20 pb-40 bg-white shadow-md rounded-md">
+                <p class="text-red-500 text-center text-xl font-bold">Kerja Kami</p>
+                <p class="pt-3 px-40 text-justify">Menyediakan Fasilitas sekolah ( Sekolah Dasar dan TK) Merenovasi Sekolah serta fasilitas pendukung lainnya, Menyediakan APE dalam sebagai  Alat Edukatif. Advokasi serta mendukung staff pengajar prihal digitalisasi edukasi termasuk Prokes di sekolah
+</p>
+
+    </div>
 
 
 
 
-    <?php include 'footer.php'; ?>
+    <?php include 'layout/footer.php'; ?>
