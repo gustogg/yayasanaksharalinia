261ebf3e74a11e93e6d62cd8b400c5ac7a90f74c
Gustoc <56625312+gustogg@users.noreply.github.com>
2021-11-07 20:37:16 +0700

    tentangkami

---
+++ tentangkami.php
@@ -0,0 +1,20 @@
+<?php include 'layout/header.php'; ?>
+<title>Home</title>
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+<style>
+    .swiper-pagination-bullet-active {
+        background-color: red;
+    }
+</style>
+</head>
+
+<body>
+    <?php include 'layout/navbar.php' ?>
+    <div class="pt-40 pb-40">
+                <p class="text-sm text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+
+
+
+
+    <?php include 'footer.php'; ?>
