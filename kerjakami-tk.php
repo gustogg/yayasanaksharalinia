d9d45c054107262deecb63f2009bf63b249ee462
yogamahhendra <yoga.mahendra2000@gmail.com>
2021-11-08 15:47:30 +0800

    Update kerjakami-tk.php

--- kerjakami-tk.php
+++ kerjakami-tk.php
@@ -6,8 +6,36 @@
     <div class="pt-16">
         <div class="">
             <img class="object-cover w-full h-60 z-10" src="https://source.unsplash.com/collection/190728/3000x900" alt="image" />
-            <div class="bg-black -mt-12 mb-12 z-30">
-                <p class="p-3 font-semibold text-center">Kerja Kami (Taman Kanak-Kanak)</p>
+            <div class="bg-black -mt-12">
+                <p class="p-3 font-semibold text-white text-center">Kerja Kami (Taman Kanak-Kanak)</p>
+            </div>
+        </div>
+        <div class="px-4 bg-gray-100">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-0">
+                <div class="grid col-span-1 p-6">
+                    <button class="hover:border-red-500 border-2 rounded-md border-gray-100">
+                        <div class="col-span-1 p-3 bg-white shadow-md rounded-md">
+                            <img class="object-cover w-screen h-52" src="public/sd/sdn7gianyar_rakbuku.jpg">
+                            <p class="pl-2 mt-2 text-sm text-left font-semibold">Rak Buku Gianyar</p>
+                        </div>
+                    </button>
+                </div>
+                <div class="grid col-span-1 p-6">
+                    <button class="hover:border-red-500 border-2 rounded-md border-gray-100">
+                        <div class="col-span-1 p-3 bg-white shadow-md rounded-md">
+                            <img class="object-cover w-screen h-52" src="public/sd/sdn7gianyar_rakbuku.jpg">
+                            <p class="pl-2 mt-2 text-sm text-left font-semibold">Rak Buku Gianyar</p>
+                        </div>
+                    </button>
+                </div>
+                <div class="grid col-span-1 p-6">
+                    <button class="hover:border-red-500 border-2 rounded-md border-gray-100">
+                        <div class="col-span-1 p-3 bg-white shadow-md rounded-md">
+                            <img class="object-cover w-screen h-52" src="public/sd/sdn7gianyar_rakbuku.jpg">
+                            <p class="pl-2 mt-2 text-sm text-left font-semibold">Rak Buku Gianyar</p>
+                        </div>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
