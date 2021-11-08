a4adba183a56c61ad7ce79d59e64b3c5184d436f
yogamahhendra <yoga.mahendra2000@gmail.com>
2021-11-08 14:13:44 +0800

    adding something

---
+++ kerjakami-sd.php
@@ -0,0 +1,10 @@
+<?php include 'layout/header.php'; ?>
+</head>
+
+<body>
+    <?php include 'layout/navbar.php' ?>
+
+
+
+    <!--Footer -->
+    <?php include 'layout/footer.php'; ?>
---
+++ kerjakami-tk.php
@@ -0,0 +1,17 @@
+<?php include 'layout/header.php'; ?>
+</head>
+
+<body>
+    <?php include 'layout/navbar.php' ?>
+    <div class="pt-16">
+        <div class="">
+            <img class="object-cover w-full h-60 z-10" src="https://source.unsplash.com/collection/190728/3000x900" alt="image" />
+            <div class="bg-black -mt-12 mb-12 z-30">
+                <p class="p-3 font-semibold text-center">Kerja Kami (Taman Kanak-Kanak)</p>
+            </div>
+        </div>
+    </div>
+
+
+    <!--Footer -->
+    <?php include 'layout/footer.php'; ?>
