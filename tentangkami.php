01f181c08d470775af1692d46d9fa779560aa28b
Gustoc <56625312+gustogg@users.noreply.github.com>
2021-11-11 02:24:44 +0700

    Update tentangkami.php

--- tentangkami.php
+++ tentangkami.php
@@ -25,6 +25,33 @@
                 <p class="pt-3 px-40 text-justify pb-10">Yayasan Akshara Linia Indonesia adalah Yayasan yang bergerak dibidang sosial, bidang kemanusiaan dengan akta Notaris Amaliyah, SH., M.KN., Nomor 13 tanggal 09 Maret 2021 dan Keputusan Mentri Hukum Dan Hak Asasi Manusia Republik Indonesia, Nomor AHU-0010105.AH.01.12 Tahun 2021 tanggal 17 Maret 2021 serta Nomor Induk Berusaha (NIB) 1238000301296 tanggal 19 Maret 2021.</p>
 
     </div>
+    <div class="mt-10 mb-10 bg-white shadow-md rounded-md">
+                <p class="text-red-500 text-center text-xl font-bold">Visi</p>
+                <p class="pt-3 px-40 text-center pb-10">Melalui Yayasan Akshara Linia Indonesia, anak anak sekolah dasar berhak mendapatkan Pendidikan dan fasilitas yang layak</p>
+
+    </div>
+    <div class="mt-10 mb-10 bg-white shadow-md rounded-md">
+                <p class="text-red-500 text-center text-xl font-bold">Misi</p>
+                <p class="pt-3 px-40 text-center pb-10">Sebagai garda terdepan untuk sosial dan kemanusiaan, dimana seribu anak yang mampu boleh mengecap Pendidikan, tetapi jangan sampai satu pun anak yang tidak mampu tidak bisa mengecap Pendidikan karena orang tuanya tidak mampu untuk membayar biaya Pendidikan karena anak yang tidak dapat makan dan anak yang tidak dapat Pendidikan sama sama bertentangan dengan nilai kemanusiaan</p>
+
+    </div>
+    </div>
+    <div class="mt-10 mb-10 bg-white shadow-md rounded-md">
+                <p class="text-red-500 text-center text-xl font-bold">Our Motto</p>
+                <p class="pt-3 px-40 text-center pb-10 font-bold font-style: italic">“WE WANT TO WORK WHILE RUNNING, WE WANT TO GIVE WHILE SMILING”</p>
+
+    </div>
+    <div class="mt-10 mb-10 bg-white shadow-md rounded-md">
+                <p class="text-red-500 text-center text-xl font-bold">Our Strategy</p>
+                <ul class="pt-3 px-40 text-left pb-10 list-style-type: none">“
+                   <li>1. Target program yang diterima dan diketahui oleh masyarakat Setempat, sekolah dan dinas Pendidikan</li>
+                   <li>2. Meninjau dan mengimplementasikan kebutuhan sekolah berdasarkan skala prioritas</li>
+                   <li>3. Menjalin dan membangun sumber daya lokal</li>
+                   <li>4. Mendukung kegiatan pemerintah di bidang Pendidikan</li>
+                   <li>5. Kerjasama dengan lembaga pengambil keputusan / stake holder terkait</li>
+                </ul>
+
+    </div>
     <div class="mt-20 pb-40 bg-white shadow-md rounded-md">
                 <p class="text-red-500 text-center text-xl font-bold">Kerja Kami</p>
                 <p class="pt-3 px-40 text-justify">Menyediakan Fasilitas sekolah ( Sekolah Dasar dan TK) Merenovasi Sekolah serta fasilitas pendukung lainnya, Menyediakan APE dalam sebagai  Alat Edukatif. Advokasi serta mendukung staff pengajar prihal digitalisasi edukasi termasuk Prokes di sekolah
