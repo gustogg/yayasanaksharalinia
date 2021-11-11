31fc0dec13e47c854ad172dd54e4da250676a337
yogamahhendra <yoga.mahendra2000@gmail.com>
2021-11-11 22:26:32 +0800

    Update Kerja Kami

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php include 'layout/header.php'; ?>
+</head>
+
+<body>
+    <?php include 'layout/navbar.php' ?>
+    <div style="height:88px;">
+    </div>
+    <div class="w-10 h-10 bg-blue-400 relative z-50">
+        AAAA
+    </div> 
+    <div class="w-10 h-10 -mt-5 bg-red-400 relative z-10">
+        AA
+    </div>
+
+
+
+    <!--Footer -->
+    <?php include 'layout/footer.php'; ?>
