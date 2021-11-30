bbd979ed2252c1b5915402edb38fc7a03cabd02d
yogamahhendra <yoga.mahendra2000@gmail.com>
2021-11-30 19:58:50 +0800

    Perbaikan rapi

--- layout/footer.php
+++ layout/footer.php
@@ -6,51 +6,56 @@
         <div class="col-span-1 flex flex-col ">
             <a class="text-sm sm:text-xl font-semibold text-white w-full">Our Page List</a>
             <a href="/index.php" class="text-xs sm:text-base my-2 text-gray-300">&#8729; Home</a>
-            <a href="/tentangkami.php"  class="text-xs sm:text-base my-2 text-gray-300">&#8729; Tentang Kami</a>
-            <a href="/kerjakami-tk.php"  class="text-xs sm:text-base my-2 text-gray-300">&#8729; Kerja Kami - TK</a>
-            <a href="/kerjakami-sd.php"  class="text-xs sm:text-base my-2 text-gray-300">&#8729; Kerja Kami - SD</a>
+            <a href="/tentangkami.php" class="text-xs sm:text-base my-2 text-gray-300">&#8729; Tentang Kami</a>
+            <a href="/kerjakami-tk.php" class="text-xs sm:text-base my-2 text-gray-300">&#8729; Kerja Kami - TK</a>
+            <a href="/kerjakami-sd.php" class="text-xs sm:text-base my-2 text-gray-300">&#8729; Kerja Kami - SD</a>
             <a href="/publikasi.php" class="text-xs sm:text-base my-2 text-gray-300">&#8729; Publikasi</a>
             <a href="#" class="text-xs sm:text-base my-2 text-gray-300">&#8729; Informasi</a>
-            
+
         </div>
         <div class="col-span-1">
             <p class="text-sm sm:text-xl font-semibold text-white">Contact Us</p>
             <div class="-ml-1.5">
                 <div class="text-xs sm:text-base my-2 flex">
                     <img class="h-4 sm:h-7" src="/images/gmail.svg">
-                    <a href="mailto:aksharalinia@gmail.com"><p class="pl-1 sm:pl-1 text-gray-300">aksharalinia@gmail.com </p></a>
+                    <a href="mailto:aksharalinia@gmail.com">
+                        <p class="pl-1 sm:pl-1 text-gray-300">aksharalinia@gmail.com </p>
+                    </a>
                 </div>
                 <div class="text-xs sm:text-base my-2 flex">
                     <img class="ml-2 h-4 sm:h-7" src="/images/whatsapp.svg">
-                    <a href="https://api.whatsapp.com/send?phone=6281360689378"><p class="ml-2 text-gray-300">+62 813 6068 9378</p></a>
+                    <a href="https://api.whatsapp.com/send?phone=6281360689378">
+                        <p class="ml-2 text-gray-300">+62 813 6068 9378</p>
+                    </a>
                 </div>
                 <div class="text-xs sm:text-base my-2 flex mt-3">
                     <img class="ml-2 h-4 sm:h-7" src="/images/whatsapp.svg">
-                    <a href="https://api.whatsapp.com/send?phone=628119401814"><p class="ml-3 text-gray-300">+62 811 9401 814</p></a>
+                    <a href="https://api.whatsapp.com/send?phone=628119401814">
+                        <p class="ml-3 text-gray-300">+62 811 9401 814</p>
+                    </a>
                 </div>
             </div>
         </div>
         <div class="lg:col-span-1 hidden lg:block">
             <p class="text-sm sm:text-xl text-left font-semibold text-white">Our Location</p>
             <div class="h-40 w-72">
-                <a href="https://goo.gl/maps/tD7NcFRXL6bnsccJ7" target="_blank" rel="noopener noreferrer">
-                    <img class="my-2 rounded-md  w-full h-full object-cover" src="/public/gmaps-lg.png">
-                </a>
+                <iframe class="h-48 w-72 my-2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.8643351916844!2d115.17976771470492!3d-8.704430991108117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd24702969aea2d%3A0x2b4b6cb0f8447ce7!2sYayasan%20Akshara%20Linia%20Indonesia!5e0!3m2!1sid!2sid!4v1638271739717!5m2!1sid!2sid" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
-
         </div>
     </div>
-    <!-- <div class="grid grid-cols-3 gap-1 sm:gap-4 object-top px-8 sm:px-32 lg:ml-0 pb-4 -mt-3 sm:-mb-2 sm:-mt-5 sm:pb-8 w-screen -ml-8 md:ml-0">
+    <div class="grid grid-cols-3 gap-1 sm:gap-4 object-top px-8 sm:px-32 lg:ml-0 pb-4 -mt-3 sm:-mb-2 sm:-mt-5 sm:pb-8 w-screen -ml-8 md:ml-0">
         <div class=" col-start-2 col-span-2 visible lg:hidden">
             <p class="text-sm sm:text-xl font-semibold text-white">Our Location</p>
-            <a href="https://goo.gl/maps/tD7NcFRXL6bnsccJ7" target="_blank" rel="noopener noreferrer">
-                <img class="rounded-md w-60 h-24 sm:w-96 sm:h-44 object-cover" src="public/gmaps-lg.png">
-            </a>
+            <iframe class="w-56 h-40 sm:w-85 sm:h-52 my-2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.8643351916844!2d115.17976771470492!3d-8.704430991108117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd24702969aea2d%3A0x2b4b6cb0f8447ce7!2sYayasan%20Akshara%20Linia%20Indonesia!5e0!3m2!1sid!2sid!4v1638271739717!5m2!1sid!2sid" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
-    </div> -->
-    <div class="text-center px-auto py-2 sm:py-3 bg-red-900 text-gray-300 font-bold text-sm sm:text-xs">
-        <p>Copyright &copy; Yayasan Akshara Linia Indonesia 2021</p>
+
+        <!-- <img class="rounded-md w-60 h-24 sm:w-96 sm:h-44 object-cover" src="public/gmaps-lg.png"> -->
+        </a>
     </div>
+</div>
+<div class="text-center px-auto py-2 sm:py-3 bg-red-900 text-gray-300 font-bold text-sm sm:text-xs">
+    <p>Copyright &copy; Yayasan Akshara Linia Indonesia 2021</p>
+</div>
 </div>
 
 </body>
