e6ac99f540bb142ad9b9b7f5f84383ad0244a973
yogamahhendra <yoga.mahendra2000@gmail.com>
2021-12-05 14:48:33 +0800

    Update navbar.php

--- layout/navbar.php
+++ layout/navbar.php
@@ -13,7 +13,7 @@
     </div>
     <nav :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row">
       <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-white focus:text-white hover:bg-red-500 focus:outline-none focus:shadow-outline" href="/tentangkami.php">Tentang Kami</a>
-      <div @click.away="open = false" class="relative font-poppins" x-data="{ open: false }">
+      <div @click.away="open = false" class="relative" x-data="{ open: false }">
         <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-white focus:text-white hover:bg-red-500 focus:bg-red-500 focus:outline-none focus:shadow-outline">
           <span>Kerja Kami</span>
           <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
